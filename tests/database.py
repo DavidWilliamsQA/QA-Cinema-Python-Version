aef480d3228b4166fd070c14b2dd19f8948f6f18
DavidWilliamsQA <60790661+DavidWilliamsQA@users.noreply.github.com>
2022-02-28 23:13:09 +0000

    added conftest file which will contain all the fixtures that we would need in the other test files

---
+++ tests/database.py
@@ -0,0 +1,41 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+from app.database import get_db, Base
+import pytest
+
+
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USERNAME')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST_NAME')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DATABASE_NAME')}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="module")
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(scope="module")
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,10 +1,18 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json().get("message"))
     assert res.json().get("message") == "Welcome to my QA Cinema App Backend"
+
+
+def test_create_movie(client):
+    res = client.post(
+        "/movies/", json={"status": "Test", "api_ID": 3, "rating": "3 stars"}
+    )
+    print(res.json())
+
+
+def test_login_user(client):
+    res = client.post(
+        "/login", data={"username": "test@test.com", "password": "password123"}
+    )
+    assert res.status_code == 200
