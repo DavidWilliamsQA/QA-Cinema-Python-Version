4dac35f071b5293b3d6572d1cce84a9f25bd7828
DavidWilliamsQA <60790661+DavidWilliamsQA@users.noreply.github.com>
2022-03-06 12:34:02 +0000

    finished testing the movie routes

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,17 +1,20 @@
-def test_create_movie(client):
-    res = client.post(
+def test_create_movie(authorized_client):
+    res = authorized_client.post(
         "/movies/", json={"status": "Test", "api_ID": 3, "rating": "3 stars"}
     )
-    print(res.json())
+    assert res.status_code == 200
 
 
-def test_delete_movie(client):
-    pass
+def test_delete_movie(authorized_client, test_movies):
+    res = authorized_client.delete(f"/movies/{test_movies[0].id}")
+    assert res.status_code == 204
 
 
-def test_get_movie_by_id(client):
-    pass
+def test_get_movie_by_id(client, test_movies):
+    res = client.get(f"/movies/{test_movies[0].id}")
+    assert res.status_code == 200
 
 
 def test_get_all_movies(client):
-    pass
+    res = client.get(f"/movies/")
+    assert res.status_code == 200
