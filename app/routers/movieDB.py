7586089251a8ea31f717047e18ac97558355b78b
DavidWilliamsQA <60790661+DavidWilliamsQA@users.noreply.github.com>
2022-04-13 11:14:41 +0100

    added route for the trailer video sources added route for the OMD movie details

--- app/routers/movieDB.py
+++ app/routers/movieDB.py
@@ -20,3 +20,21 @@ def get_movie(id: int):
     )
     print(res.json())
     return res.json()
+
+
+@router.get("/video/{id}")
+def get_movie(id: int):
+    res = requests.get(
+        f"https://api.themoviedb.org/3/movie/{id}/videos?api_key={os.getenv('MOVIE_DB_API_KEY')}&language=en-UK"
+    )
+    print(res.json())
+    return res.json()
+
+
+@router.get("/omb/{title}")
+def get_movie(title: str):
+    res = requests.get(
+        f"http://www.omdbapi.com/?apikey={os.getenv('OMD_MOVIE_DB_API_KEY')}&t={title}"
+    )
+    print(res.json())
+    return res.json()
